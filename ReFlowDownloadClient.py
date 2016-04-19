e940b535a30367e561c11255c0491cb54b339e78
Scott White <whitews@gmail.com>
2016-04-19 18:42:20 -0400

    fix widget spacing and replace site panel references with panel template

--- ReFlowDownloadClient.py
+++ ReFlowDownloadClient.py
@@ -86,7 +86,7 @@ class Application(Tkinter.Frame):
         self.site_dict = dict()
         self.subject_dict = dict()
         self.visit_dict = dict()
-        self.site_panel_dict = dict()
+        self.panel_template_dict = dict()
         self.stimulation_dict = dict()
 
         # dict of ChosenFile objects, key is file path, value is ChosenFile
@@ -99,7 +99,6 @@ class Application(Tkinter.Frame):
 
         self.site_menu = None
         self.site_selection = Tkinter.StringVar()
-        self.site_selection.trace("w", self.update_site_metadata)
 
         self.subject_menu = None
         self.subject_selection = Tkinter.StringVar()
@@ -110,8 +109,8 @@ class Application(Tkinter.Frame):
         self.stimulation_menu = None
         self.stimulation_selection = Tkinter.StringVar()
 
-        self.site_panel_menu = None
-        self.site_panel_selection = Tkinter.StringVar()
+        self.panel_template_menu = None
+        self.panel_template_selection = Tkinter.StringVar()
 
         # can't call super on old-style class, call parent init directly
         Tkinter.Frame.__init__(self, master)
@@ -251,7 +250,9 @@ class Application(Tkinter.Frame):
         top_frame.pack(
             fill='x',
             expand=False,
-            anchor='n'
+            anchor='n',
+            padx=PAD_MEDIUM,
+            pady=PAD_MEDIUM
         )
 
         middle_frame = Tkinter.Frame(main_frame, bg=BACKGROUND_COLOR)
@@ -266,8 +267,8 @@ class Application(Tkinter.Frame):
             fill='x',
             expand=False,
             anchor='n',
-            padx=0,
-            pady=0
+            padx=PAD_MEDIUM,
+            pady=PAD_MEDIUM
         )
 
         # Action buttons all go in top frame
@@ -286,7 +287,9 @@ class Application(Tkinter.Frame):
         middle_left_frame.pack(
             fill='both',
             expand=False,
-            side='left'
+            side='left',
+            padx=PAD_MEDIUM,
+            pady=PAD_MEDIUM
         )
 
         middle_right_frame = Tkinter.Frame(
@@ -296,22 +299,37 @@ class Application(Tkinter.Frame):
         middle_right_frame.pack(
             fill='both',
             expand=True,
-            side='right'
+            side='right',
+            padx=PAD_MEDIUM,
+            pady=PAD_MEDIUM
         )
 
-        # Metadata frame - for choosing project/subject/site etc.
-        metadata_frame = Tkinter.LabelFrame(
+        # Metadata label frame - wrapper for metadata frame to allow padding
+        # seems internal padding doesn't work?
+        metadata_label_frame = Tkinter.LabelFrame(
             middle_left_frame,
+            bg=BACKGROUND_COLOR
+        )
+        metadata_label_frame.pack(
+            fill='x',
+            expand=False,
+            anchor='n',
+            pady=(0, PAD_LARGE)
+        )
+        metadata_label_frame.config(text="FCS Metadata Filters")
+
+        # Metadata frame - for choosing project/subject/site etc.
+        metadata_frame = Tkinter.Frame(
+            metadata_label_frame,
             bg=BACKGROUND_COLOR
         )
         metadata_frame.pack(
             fill='x',
             expand=False,
             anchor='n',
-            ipadx=PAD_MEDIUM,
-            ipady=PAD_MEDIUM
+            padx=PAD_MEDIUM,
+            pady=PAD_MEDIUM
         )
-        metadata_frame.config(text="Filter FCS Files")
 
         # Download options frame
         download_options_frame = Tkinter.LabelFrame(
@@ -320,10 +338,8 @@ class Application(Tkinter.Frame):
         )
         download_options_frame.pack(
             fill='both',
-            expand=False,
-            anchor='n',
-            ipadx=PAD_MEDIUM,
-            ipady=PAD_MEDIUM
+            expand=True,
+            anchor='n'
         )
         download_options_frame.config(text="Download Options")
 
@@ -449,6 +465,38 @@ class Application(Tkinter.Frame):
 
         visit_frame.pack(side='top', fill='x', expand=True)
 
+        # overall panel_template frame
+        panel_template_frame = Tkinter.Frame(
+            metadata_frame,
+            bg=BACKGROUND_COLOR)
+
+        # panel_template label frame
+        panel_template_chooser_label_frame = Tkinter.Frame(
+            panel_template_frame,
+            bg=BACKGROUND_COLOR)
+        panel_template_chooser_label = Tkinter.Label(
+            panel_template_chooser_label_frame,
+            text='Panel Template:',
+            bg=BACKGROUND_COLOR,
+            width=LABEL_WIDTH,
+            anchor=Tkinter.E)
+        panel_template_chooser_label.pack(side='left')
+        panel_template_chooser_label_frame.pack(side='left', fill='x')
+
+        # panel_template chooser listbox frame
+        panel_template_chooser_frame = Tkinter.Frame(
+            panel_template_frame,
+            bg=BACKGROUND_COLOR)
+        self.panel_template_menu = Tkinter.OptionMenu(
+            panel_template_chooser_frame,
+            self.panel_template_selection,
+            '')
+        self.panel_template_menu.config(bg=BACKGROUND_COLOR)
+        self.panel_template_menu.pack(fill='x', expand=True)
+        panel_template_chooser_frame.pack(fill='x', expand=True)
+
+        panel_template_frame.pack(side='top', fill='x', expand=True)
+
         # overall stimulation frame
         stimulation_frame = Tkinter.Frame(
             metadata_frame,
@@ -480,38 +528,6 @@ class Application(Tkinter.Frame):
         stimulation_chooser_frame.pack(fill='x', expand=True)
 
         stimulation_frame.pack(side='top', fill='x', expand=True)
-
-        # overall site_panel frame
-        site_panel_frame = Tkinter.Frame(
-            metadata_frame,
-            bg=BACKGROUND_COLOR)
-
-        # site_panel label frame
-        site_panel_chooser_label_frame = Tkinter.Frame(
-            site_panel_frame,
-            bg=BACKGROUND_COLOR)
-        site_panel_chooser_label = Tkinter.Label(
-            site_panel_chooser_label_frame,
-            text='Site Panel:',
-            bg=BACKGROUND_COLOR,
-            width=LABEL_WIDTH,
-            anchor=Tkinter.E)
-        site_panel_chooser_label.pack(side='left')
-        site_panel_chooser_label_frame.pack(side='left', fill='x')
-
-        # site_panel chooser listbox frame
-        site_panel_chooser_frame = Tkinter.Frame(
-            site_panel_frame,
-            bg=BACKGROUND_COLOR)
-        self.site_panel_menu = Tkinter.OptionMenu(
-            site_panel_chooser_frame,
-            self.site_panel_selection,
-            '')
-        self.site_panel_menu.config(bg=BACKGROUND_COLOR)
-        self.site_panel_menu.pack(fill='x', expand=True)
-        site_panel_chooser_frame.pack(fill='x', expand=True)
-
-        site_panel_frame.pack(side='top', fill='x', expand=True)
 
         self.load_user_projects()
 
@@ -562,9 +578,8 @@ class Application(Tkinter.Frame):
         progress_frame.pack(
             fill='x',
             expand=False,
-            anchor='s',
-            padx=PAD_MEDIUM,
-            pady=PAD_SMALL)
+            anchor='s'
+        )
 
     def _on_mousewheel(self, event):
         self.file_list_canvas.yview_scroll(-event.delta, "units")
@@ -589,7 +604,7 @@ class Application(Tkinter.Frame):
         self.subject_menu['menu'].delete(0, 'end')
         self.visit_menu['menu'].delete(0, 'end')
         self.stimulation_menu['menu'].delete(0, 'end')
-        self.site_panel_menu['menu'].delete(0, 'end')
+        self.panel_template_menu['menu'].delete(0, 'end')
 
         for result in response['data']:
             self.project_dict[result['project_name']] = result['id']
@@ -674,7 +689,7 @@ class Application(Tkinter.Frame):
                 command=lambda value=visit_type_name:
                 self.visit_selection.set(value))
 
-    def load_stimulations(self, project_id):
+    def load_project_stimulations(self, project_id):
         self.stimulation_menu['menu'].delete(0, 'end')
         self.stimulation_selection.set('')
         self.stimulation_dict.clear()
@@ -699,33 +714,30 @@ class Application(Tkinter.Frame):
                 command=lambda value=stimulation:
                 self.stimulation_selection.set(value))
 
-    def update_site_metadata(self, *args, **kwargs):
-        self.site_panel_menu['menu'].delete(0, 'end')
-        self.site_panel_selection.set('')
-        self.site_panel_dict.clear()
+    def load_project_panel_templates(self, project_id):
+        self.panel_template_menu['menu'].delete(0, 'end')
+        self.panel_template_selection.set('')
+        self.panel_template_dict.clear()
 
-        if not self.site_selection.get():
-            return
-        site_pk = self.site_dict[self.site_selection.get()]
-        rest_args = [self.host, self.token]
-        rest_kwargs = {'site_pk': site_pk}
-
+        response = None
         try:
-            response = rest.get_site_panels(*rest_args, **rest_kwargs)
+            response = rest.get_project_panels(
+                self.host,
+                self.token,
+                project_pk=project_id)
         except Exception, e:
             print e
-            return
 
         if 'data' not in response:
             return
 
         for result in response['data']:
-            self.site_panel_dict[result['name']] = result['id']
-        for panel_name in sorted(self.site_panel_dict.keys()):
-            self.site_panel_menu['menu'].add_command(
+            self.panel_template_dict[result['name']] = result['id']
+        for panel_name in sorted(self.panel_template_dict.keys()):
+            self.panel_template_menu['menu'].add_command(
                 label=panel_name,
                 command=lambda value=panel_name:
-                self.site_panel_selection.set(value))
+                self.panel_template_selection.set(value))
 
     def update_metadata(*args):
         self = args[0]
@@ -736,7 +748,8 @@ class Application(Tkinter.Frame):
             self.load_project_sites(self.project_dict[option_value])
             self.load_project_subjects(self.project_dict[option_value])
             self.load_project_visits(self.project_dict[option_value])
-            self.load_stimulations(self.project_dict[option_value])
+            self.load_project_panel_templates(self.project_dict[option_value])
+            self.load_project_stimulations(self.project_dict[option_value])
 
         self.update_add_to_queue_button_state()
 
